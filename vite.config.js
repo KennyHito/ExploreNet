1ec5d29810fdd5dbd335efe6d8e2b9ef1d61490b
 KennyHito <1154180808@qq.com>
2026-07-24 16:17:03 +0800

    增加局域网80端口访问，无需写端口号

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,11 @@ import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
   plugins: [vue()],
+  // 开发服务器相关配置
   server: {
+    // 监听所有地址（便于局域网/容器访问）
     host: true,
-    port: 5173
+    // 指定开发服务器启动的端口号，默认是5173，这里设置为80，方便在局域网中访问即无需写端口号
+    port: 80
   }
 })
